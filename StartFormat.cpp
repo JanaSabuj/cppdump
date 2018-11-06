28c969ab859cbe16ed31351454ef271e7db5947f
Sabuj Jana <sabujlovescoding@gmail.com>
2018-11-06 17:18:42 +0530

    Create StartFormat.cpp

---
+++ StartFormat.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define double long double
+
+#define loop(i,a,b) for(int i=a;i<b;i++)
+#define loopb(i,a,b) for(int i=a;i>=b;i--)
+#define loopm(i,a,b,step) for(int i=a;i<b;i+=step)
+#define loopbm(i,a,b,step) for(int i=a;i>=b;i-=step)
+
+#define pb push_back
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define endl "\n"
+
+#define _ ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define __ freopen("input.txt","r",stdin);freopen("output.txt","w",stdout)
+
+ int main(){
+  _; 
+  
+ //Code goes here
+   
+	return 0;
+}
+
+
+
+
+
+
+
+
+
