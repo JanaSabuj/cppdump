f54d673f9f3db3feb6163e4bcc9f20337aa885ed
Sabuj Jana <sabujlovescoding@gmail.com>
2019-01-11 15:13:00 +0530

    Sublime Text Setup for CP

---
+++ sublime/abc.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fileio()
+{
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+}
+
+
+int main() {
+
+	fileio();
+
+	 cout<<"GFG"<<endl;int x; cin>>x; cout<<x*x<<endl;
+
+	return 0;
+
+
+}
+
