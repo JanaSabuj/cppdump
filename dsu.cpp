846fd861fc3e7c953ccd72a637bd14a5d7417519
Sabuj Jana <sabujlovescoding@gmail.com>
2020-04-05 13:50:38 +0530

    updt.

---
+++ dsu.cpp
@@ -0,0 +1,26 @@
+// DSU with path compression and union by size
+const int N = 1e5 + 7;
+vector<int> parent(N);
+vector<int> size(N);
+
+void make_set(int v) {
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (size[a] < size[b])
+            swap(a, b);
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
