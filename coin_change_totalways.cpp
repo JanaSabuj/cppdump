f3e2a0664085da536beaee4f63001799a7498cf0
Sabuj Jana <sabujlovescoding@gmail.com>
2020-05-12 11:03:31 +0530

    Update coin_change_totalways.cpp

--- coin_change_totalways.cpp
+++ coin_change_totalways.cpp
@@ -5,15 +5,15 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 
 vi arr;
-int len, total;
+int len;
 int dp[255][500];
 
-int coin(int idx, int& store) {
-    if (store > total)
+int coin(int idx, int& store) {// store is ref variable for time optimisation
+    if (store < 0)
         return 0;
 
     if (idx == len) {
-        if (store == total)
+        if (store == 0)
             return 1;
         else {
             return 0;
@@ -23,9 +23,9 @@ int coin(int idx, int& store) {
     if (dp[idx][store] != -1)
         return dp[idx][store];
 
-    store += arr[idx];
+    store -= arr[idx];// ref variable modified
     int A = coin(idx, store);
-    store -= arr[idx];
+    store += arr[idx];// ref variable restored
 
     int B = coin(idx + 1, store);
 
@@ -42,10 +42,9 @@ void solve() {
     }
 
     len = m;
-    total = n;
-    int store = 0;
+    int store = n;
     memset(dp, -1, sizeof(dp));
-    int ways = coin(0, store);
+    int ways = coin(0, store);// traverse from total change to 0 change
 
     cout << ways << endl;
 }
