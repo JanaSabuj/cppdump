b867af5f6d3c762410038d68d7df67a11dc6802a
Sabuj Jana <sabujlovescoding@gmail.com>
2020-06-06 10:01:44 +0530

    Create Main.java

---
+++ Java/Main.java
@@ -0,0 +1,61 @@
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+class Main {
+	public static void main(String[] args) {
+		FastScanner sc = new FastScanner();
+		int t = sc.nextInt();
+		for(int i = 0; i < t; i++) {
+			 
+		}
+
+	}
+
+	public static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		public FastScanner(String s) {
+			try {
+				br = new BufferedReader(new FileReader(s));
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		public FastScanner() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String nextToken() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(nextToken());
+		}
+
+		long nextLong() {
+			return Long.parseLong(nextToken());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(nextToken());
+		}
+	}
+}
