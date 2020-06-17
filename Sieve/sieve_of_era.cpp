c9a57627fc0cb7d82a9f635da73aa877b266a9c3
Sabuj Jana <sabujlovescoding@gmail.com>
2020-06-17 13:48:28 +0530

    Update and rename sieve_of_era.cpp to Sieve/sieve_of_era.cpp

--- Sieve/sieve_of_era.cpp
+++ Sieve/sieve_of_era.cpp
@@ -1,8 +1,11 @@
 #include<bits/stdc++.h>
 using namespace std;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
 
 const int N = 1e6 + 7;
-vector<int> primes;
+vi primes;
+vvi pfs(N);// pfs[i] contains all pfs of i
 bool composite[N];// {0} initially all prime
 
 // O(N loglogN)
@@ -11,14 +14,21 @@ void sieve() {
 		// till N or sqrt(N) doesnt matter. Almost same complexity of O(NloglogN)
 		if (composite[i]) continue;
 		primes.push_back(i);// prime
-		for (int j = 2 * i; j <= N; j += i)
+		pfs[i].push_back(i);
+		for (int j = 2 * i; j <= N; j += i) {
 			composite[j] = 1;// multiple of prime
+			pfs[j].push_back(i);
+		}
 	}
 }
 
 int main() {
 	sieve();
-	for (auto x : primes)
-		cout << x << " ";
+	for (int i = 1; i <= 10; i++) {
+		cout << i << " ";
+		for (auto x : pfs[i])
+			cout << x << ",";
+		cout << endl;
+	}
 	return 0;
 }
