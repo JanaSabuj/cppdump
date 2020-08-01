feb446c6a97c6cf1f4e22bda9174a3f21d34b8c1
Sabuj Jana <sabujlovescoding@gmail.com>
2020-08-01 16:00:06 +0530

    Update ulb.cpp

--- upper_lower_bound/ulb.cpp
+++ upper_lower_bound/ulb.cpp
@@ -1,15 +1,22 @@
-signed main() {
+#include <bits/stdc++.h>
+using namespace std;
 
+typedef vector<int> vi;
+
+int main() {
+	
 	///////////0   1   2   3  	4  5   6  7
 	vi arr = {10, 20, 20, 20, 30, 40, 40, 50};
 
 	// first element greater or equal
 	cout << lower_bound(arr.begin(), arr.end(), 20) - arr.begin() << endl;// 1
 	cout << lower_bound(arr.begin(), arr.end(), 5) - arr.begin() << endl;// 0
+	cout << lower_bound(arr.begin(), arr.end(), 55) - arr.begin() << endl;// 8
 
 	// first element greater
 	cout << upper_bound(arr.begin(), arr.end(), 40) - arr.begin() << endl;// 7
 	cout << upper_bound(arr.begin(), arr.end(), 0) - arr.begin() << endl;// 0
-
+	cout << upper_bound(arr.begin(), arr.end(), 55) - arr.begin() << endl;// 8
+ 
 	return 0;
 }
