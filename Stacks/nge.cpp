7b3508f7c6c0e396e7f8f2a3c59914d5c2a9df37
Sabuj Jana <sabujlovescoding@gmail.com>
2020-08-16 19:39:20 +0530

    nge, pge, nse

--- Stacks/nge.cpp
+++ Stacks/nge.cpp
@@ -44,6 +44,28 @@ vi ngef(vi& arr){
     return nge;
 
 }
+
+vi nsef(vi& arr){
+    int n = arr.size();
+    vi nse(n);
+
+    stack<int> s;    
+    for(int i = n - 1; i >= 0; i--){
+        while(!s.empty() and arr[s.top()] >= arr[i])
+            s.pop();
+        
+        if(s.empty())
+            nse[i] = -1;
+        else
+            nse[i] = arr[s.top()];
+        
+        s.push(i);
+    }
+
+    return nse;
+}
+
+
 int main() {
     vector<int> arr = {1, 3, 5, 2, 4};
     vi nge = ngef(arr);
@@ -59,5 +81,11 @@ int main() {
 
     for(auto x: pge)
         cout << x << " ";
+
+    cout << endl;
+    vi nse = nsef(arr);
+    for(auto x: nse){
+        cout << x << " ";
+    }
     return 0;
 }
