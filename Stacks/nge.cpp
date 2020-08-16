d5f6f4781abff6a02af32fd6ab71cb23678a958f
Sabuj Jana <sabujlovescoding@gmail.com>
2020-08-16 19:35:52 +0530

    Update nge.cpp

--- Stacks/nge.cpp
+++ Stacks/nge.cpp
@@ -2,21 +2,43 @@
 using namespace std;
 typedef vector<int> vi;
 
+vi pgef(vi& arr){
+    stack<int> s;
+    int n = arr.size();    
+    vi pge(n);
+
+    // just reverse the loop
+    for(int i = 0; i < n; i++){
+        while(!s.empty() and s.top() <= arr[i])
+            s.pop();
+        
+        if(s.empty())
+            pge[i] = -1;
+        else
+            pge[i] = s.top();
+        
+        s.push(arr[i]);
+    }
+
+    return pge;
+
+}
+
 vi ngef(vi& arr){
     stack<int> s;
     int n = arr.size();
     vi nge(n);
 
     for(int i = n - 1; i >= 0; i--){
-        while(!s.empty() and s.top() <= arr[i])
+        while(!s.empty() and arr[s.top()] <= arr[i])
             s.pop();
         
         if(s.empty())
             nge[i] = -1;
         else
-            nge[i] = s.top();
+            nge[i] = arr[s.top()];
 
-        s.push(arr[i]);
+        s.push(i);
     }
 
     return nge;
@@ -25,8 +47,17 @@ vi ngef(vi& arr){
 int main() {
     vector<int> arr = {1, 3, 5, 2, 4};
     vi nge = ngef(arr);
-    
+    vi pge = pgef(arr);
+
+    for(auto x: arr)
+        cout << x << " ";
+    cout << endl;
+
     for(auto x: nge)
+        cout << x << " ";
+    cout << "\n";
+
+    for(auto x: pge)
         cout << x << " ";
     return 0;
 }
