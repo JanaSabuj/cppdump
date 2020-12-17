af1ad0e9a5339465f8ec7525a7ded92872ce2b4d
Sabuj Jana <sabujlovescoding@gmail.com>
2020-12-17 15:28:21 +0530

    Update ncrmodp.cpp

--- ncrmodp.cpp
+++ ncrmodp.cpp
@@ -1,3 +1,44 @@
+const int maxn = 2e5 + 5;
+const int mod = 1e9 + 7;
+int fac[maxn], invfac[maxn];
+
+int add(int a, int b) {
+	a += b;
+	if (a >= mod) a -= mod;
+	return a;
+}
+int mul(int a, int b) {
+	a *= b;
+	if (a >= mod)
+		a %= mod;
+	return a;
+}
+int powmod(int x, int p)
+{
+	int sum = 1;
+	for (; p; x = mul(x, x), p >>= 1)
+		if (p & 1)
+			sum = mul(sum, x);
+	return sum;
+}
+int ncr(int n, int r)
+{
+	if (r < 0)
+		return 0;
+	if (r > n)
+		return 0;
+	return mul(mul(fac[n], invfac[n - r]), invfac[r]);
+}
+void preprocess() {
+	fac[0] = 1;
+	for (int i = 1; i < maxn; ++i) {
+		fac[i] = mul(fac[i - 1], i);
+	}
+	invfac[maxn - 1] = powmod(fac[maxn - 1], mod - 2);
+	for (int i = maxn - 2; i >= 0; i--)
+		invfac[i] = mul(invfac[i + 1], i + 1);
+}
+--------------------------------------------------------------------------------------------------------------------------------------------
 const int lim = 1e6;
 const int p = 1e9 + 7;
 int fac[lim];
@@ -114,3 +155,4 @@ signed main() {
 
 	return 0;
 }
+-------------------------------------------------------------------------------------------------------------------------------------------------
