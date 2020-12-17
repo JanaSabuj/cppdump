fbc845a18cf35dc367f0d203a8e0b5eafdd333c6
Sabuj Jana <sabujlovescoding@gmail.com>
2020-12-17 22:29:24 +0530

    Create binpow.cpp

---
+++ binpow.cpp
@@ -0,0 +1,22 @@
+int binpow(int a, int b) {
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+
+int binpow(int a, int b, int m) {
+    a %= m;
+    int res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+} 
