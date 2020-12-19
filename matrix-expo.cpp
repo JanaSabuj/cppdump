db336800ed4b52ec020105bcd53d96f8f236499d
Sabuj Jana <sabujlovescoding@gmail.com>
2020-12-19 18:42:31 +0530

    Update matrix-expo.cpp

--- matrix-expo.cpp
+++ matrix-expo.cpp
@@ -1,57 +1,71 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define crap ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define int long long int
-#define double long double
-typedef vector<int> vi;
-typedef vector<vector<int>> vvi;
-#define endl "\n"
-void print1d(const vector<int>& vec) {for (auto val : vec) {cout << val << " ";} cout << endl;}
-void print2d(const vector<vector<int>>& vec) {for (auto row : vec) {for (auto val : row) {cout << val << " ";} cout << endl;}}
+int addx(int a, int b) {
+	a += b;
+	if (a >= mod) a -= mod;
+	return a;
+}
+int mulx(int a, int b) {
+	a *= b;
+	if (a >= mod)
+		a %= mod;
+	return a;
+}
+
 
 vvi mul(vvi& a, vvi& b) {
-	vvi prod = {{0, 0}, {0, 0}};
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 2; j++) {
-			for (int k = 0; k < 2; k++)
-				prod[i][j] += (a[i][k] * b[k][j]);
+	// a = n * k, b = k * m
+	int n = a.size();
+	int m = b[0].size();
+	int kk = a[0].size();
+
+	vvi prod(n, vi(m, 0));;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int k = 0; k < kk; k++)
+				prod[i][j] = addx(prod[i][j],  mulx(a[i][k],  b[k][j]));
 		}
 	}
 
 	return prod;
 }
 
-vvi matrix_expo(vvi a, int n) {
-	vvi res = {{1, 0}, {0 , 1}};// I2
-	while (n > 0) {
-		if (n & 1)
+vvi matrix_expo(vvi& a, int x) {
+	int n = a.size();
+	int m = a[0].size();
+	vvi res(n, vi(m, 0));
+	for (int i = 0; i < m; ++i) {
+		res[i][i] = 1;
+	}
+	// vvi res = {{1, 0}, {0 , 1}};// I2
+	while (x > 0) {
+		if (x & 1)
 			res = mul(res, a);
 		a = mul(a, a);
-		n /= 2;
+		x /= 2;
 	}
 
 	return res;
 }
 
-signed main() {
-	crap;
-	int n = 1e2;
-	vi fib2;
 
-	// matrix-expo in logN
-	vvi base = {
-		{1, 1},
-		{1, 0}
-	};
+void solve() {
+	// An = A * P
+	int n;
+	cin >> n;
+	vvi A = {{1, 1}, {1, 0}};
+	vi P = {0, 1};
 
-	for (int i = 0; i < n; i++) {
-		vvi t = matrix_expo(base, i);
-		fib2.push_back(t[0][0] * 1 + t[1][0] * 0);
-		if (fib2[i] > (int)1e10)
-			break;
+
+	if (n < 2)
+		cout << P[n] << endl;
+	else {
+
+		vvi t = matrix_expo(A, n - 1);
+		int ans = 0;
+		int cols = A[0].size();
+
+		for (int i = 0; i < cols; i++) {
+			ans = addx(ans , mulx(t[0][i] , P[2 - i - 1]));
+		}
+		cout << ans << endl;
 	}
-
-	// print1d(fib2);
-
-	return 0;
 }
