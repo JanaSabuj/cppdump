1fc30a53e8da8235f329db429c19ebc32249cd1b
Sabuj Jana <sabujlovescoding@gmail.com>
2021-03-04 21:21:24 +0530

    Update unionfind.cpp

--- unionfind/unionfind.cpp
+++ unionfind/unionfind.cpp
@@ -1,39 +1,44 @@
 const int N = 1e5;
-vi parent;
-vi _size;
 
-void make_set(int v) {
-	parent[v] = v;
-	_size[v] = 1;
-}
+struct dsu {
+	vector<int> parent;
+	vector<int> _size;
 
-// Path compression - O(log N)
-int find_set(int v) {
-	if (v == parent[v])
-		return v;
-	return parent[v] = find_set(parent[v]);
-}
+	void union_sets(int a, int b) {
+		a = find_set(a);
+		b = find_set(b);
+		if (a != b) {
+			if (_size[a] < _size[b])
+				swap(a, b);
+			parent[b] = a;
+			_size[a] += _size[b];
+		}
+	}
+
+	int find_set(int v) {
+		if (v == parent[v])
+			return v;
+		return parent[v] = find_set(parent[v]);
+	}
+
+	void make_set(int v) {
+		parent[v] = v;
+		_size[v] = 1;
+	}
+
+	void _init(int n) {
+		parent.assign(n, 0);
+		_size.assign(n, 0);
+
+		for (int i = 0; i < n; i++)
+			make_set(i);
+	}
+};
 
 // Union by Size - O(alphaN)
 // a big, b small
-void union_sets(int a, int b) {
-	a = find_set(a);
-	b = find_set(b);
-	if (a != b) {
-		if (_size[a] < _size[b])
-			swap(a, b);
-		parent[b] = a;
-		_size[a] += _size[b];
-	}
-}
 
-void _init(int n){
-	parent.assign(n, 0);
-	_size.assign(n, 0);
-	
-	for(int i = 0; i < n; i++)
-		make_set(i);
-}
+// Path compression - O(log N)
 
 // int find_set(int v) {
 // 	if (v == parent[v])
