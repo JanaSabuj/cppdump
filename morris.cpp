92bc141ae327ce01b480fde7a10ffb0be0ba401d
Sabuj Jana <sabujlovescoding@gmail.com>
2021-11-12 10:31:55 +0530

    Update morris.cpp

--- morris.cpp
+++ morris.cpp
@@ -4,21 +4,25 @@ public:
         vector<int> nodes;
         while (root) {
             if (root -> left) {
+                // find inorder predeccsor
                 TreeNode* pre = root -> left;
                 while (pre -> right && pre -> right != root) {
                     pre = pre -> right;
                 }
+                // if not visited before, so rhs is null
                 if (!pre -> right) {
-                    pre -> right = root;
-                    root = root -> left;
+                    pre -> right = root;// link to curr node before
+                    root = root -> left;// going left
                 } else {
+                    // visited before, so now destroy the link
                     pre -> right = NULL;
-                    nodes.push_back(root -> val);
-                    root = root -> right;
+                    nodes.push_back(root -> val);// push it
+                    root = root -> right;// left done, self done , now go right
                 }
             } else {
-                nodes.push_back(root -> val);
-                root = root -> right;
+                // left completed
+                nodes.push_back(root -> val);// push it
+                root = root -> right;// go right
             }
         }
         return nodes;
